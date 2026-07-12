b17aff216c52cfe457e017817b71562b666b1173
Conke Hu <conke.hu@maxwit.com>
2026-07-13 01:41:52 +0800

    enable mDNS
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- general/scripts/patch-firmware.sh
+++ general/scripts/patch-firmware.sh
@@ -2,7 +2,7 @@
 
 vendor_list=${1:-"hisilicon goke sigmastar"}
 
-# Vendor-specific
+# Vendor-specific Fix
 
 for vendor in $vendor_list
 do
@@ -25,12 +25,23 @@ do
         xc="BR2_TARGET_ROOTFS_UBIFS=y"
         grep -q ^$xc $conf || sed -i '/^BR2_TARGET_ROOTFS_CPIO=y/a BR2_TARGET_ROOTFS_UBIFS=y' $conf
     done
+
+    if [ $vendor == sigmastar ]; then
+        sed -i 's/^\(\s\+detect_sensor\)/# \1/' \
+            general/package/sigmastar-osdrv-infinity*/files/script/load_sigmastar
+    fi
 done
 
-# Common
+# Common Fix
 
 xc='$MODALIAS=usb:v0BDApF179d0000dc00dsc00dp00icFFiscFFipFFin00 root:root 660 @modprobe $MODALIAS'
 conf=general/overlay/etc/mdev.conf
 grep -q v0BDApF179 $conf || echo "$xc" >> $conf
 
 sed -i 's/\(^[^#]*ifconfig.*fw_printenv -n ipaddr.*\)/# \1/' general/overlay/usr/share/udhcpc/default.script
+
+grep -q BR2_PACKAGE_MDNSD general/openipc.fragment || cat >> general/openipc.fragment << __EOF__
+
+BR2_PACKAGE_MDNSD_OPENIPC=y
+BR2_PACKAGE_MDNSD_MQUERY_OPENIPC=y
+__EOF__
