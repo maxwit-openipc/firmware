ffd3c23a4e5f51cd9bfc651b575ac3b226e77fbd
Conke Hu <conke.hu@maxwit.com>
2026-07-12 22:25:39 +0800

    init add
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

---
+++ general/overlay/usr/share/openipc/customizer.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+SN=$(ipcinfo -i)
+SN=${SN:0:12}
+
+if [ ! -z "$SN" ]; then
+	HOSTNAME="IPC-$SN"
+	hostname "$HOSTNAME"
+	echo "$HOSTNAME" > /etc/hostname
+	echo "127.0.0.1 localhost" > /etc/hosts
+	echo "127.0.1.1 ${HOSTNAME}" >> /etc/hosts
+fi
+
+wled_pin=$(gpiofind wled_pin | awk '{print $2}')
+test -n "$wled_pin" && yaml-cli -s .nightMode.backlightPin $wled_pin
+
+ircut_pin1=$(gpiofind ircut_pin1 | awk '{print $2}')
+ircut_pin2=$(gpiofind ircut_pin2 | awk '{print $2}')
+test -n "$ircut_pin1" && yaml-cli -s .nightMode.irCutPin1 $ircut_pin1
+test -n "$ircut_pin2" && yaml-cli -s .nightMode.irCutPin2 $ircut_pin2
